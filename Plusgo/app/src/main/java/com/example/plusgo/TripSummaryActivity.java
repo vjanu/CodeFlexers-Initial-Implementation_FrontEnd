87641f3a330be031312202b55c92fd38a3e37a95
surathruwan <surathruwan@gmail.com>
2019-04-30 09:53:50 +0530

    Initial Commit with Trip Summary

---
+++ Plusgo/app/src/main/java/com/example/plusgo/TripSummaryActivity.java
@@ -0,0 +1,21 @@
+/*
+ * *
+ *  * Created by Ashane Edirisinghe
+ *  * Copyright (c) 2019 . All rights reserved.
+ *  * Last modified 4/30/19 9:47 AM
+ *
+ */
+
+package com.example.plusgo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TripSummaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trip_summary);
+    }
+}
---
+++ Plusgo/app/src/main/java/com/example/plusgo/UserProfile.java
@@ -0,0 +1,21 @@
+/*
+ * *
+ *  * Created by Ashane Edirisinghe
+ *  * Copyright (c) 2019 . All rights reserved.
+ *  * Last modified 4/30/19 9:50 AM
+ *
+ */
+
+package com.example.plusgo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+    }
+}
