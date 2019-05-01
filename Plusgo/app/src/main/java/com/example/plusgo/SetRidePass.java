5bbd77ff0a9b4e55bc79fa7db014b18278672c85
athnath <32517652+athnath@users.noreply.github.com>
2019-05-01 14:23:31 +0530

    add button

--- Plusgo/app/src/main/java/com/example/plusgo/SetRidePass.java
+++ Plusgo/app/src/main/java/com/example/plusgo/SetRidePass.java
@@ -1,12 +1,39 @@
 /*
  * *
- *  * Created by Ashane Edirisinghe
- *  * Copyright (c) 2019 . All rights reserved.
- *  * Last modified 5/1/19 2:01 PM
+ *  * Created by Athrie
+ *  *
+ *  *
  *
  */
 
 package com.example.plusgo;
 
-public class SetRidePass {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class SetRidePass extends AppCompatActivity {
+
+    EditText srctxt,destxt;
+    Button search_btn;
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setride);
+
+        srctxt=findViewById(R.id.srctxt);
+        destxt=findViewById(R.id.destxt);
+        search_btn=findViewById(R.id.search_btn);
+        search_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
 }
