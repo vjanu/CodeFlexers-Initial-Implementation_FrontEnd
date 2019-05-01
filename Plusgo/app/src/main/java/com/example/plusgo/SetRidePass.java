c9a73cab7dda8ad04e7f24191a79e70b81404880
athnath <32517652+athnath@users.noreply.github.com>
2019-05-01 14:04:35 +0530

    add set ride file

---
+++ Plusgo/app/src/main/java/com/example/plusgo/SetRidePass.java
@@ -0,0 +1,12 @@
+/*
+ * *
+ *  * Created by Ashane Edirisinghe
+ *  * Copyright (c) 2019 . All rights reserved.
+ *  * Last modified 5/1/19 2:01 PM
+ *
+ */
+
+package com.example.plusgo;
+
+public class SetRidePass {
+}
