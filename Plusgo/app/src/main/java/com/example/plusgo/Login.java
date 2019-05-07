6b9c59c356a9bf88755b72b737adb4f029fd0777
vjanu <vjanuradhawick@gmail.com>
2019-05-07 20:43:04 +0530

    user addition added

--- Plusgo/app/src/main/java/com/example/plusgo/Login.java
+++ Plusgo/app/src/main/java/com/example/plusgo/Login.java
@@ -9,14 +9,32 @@
 
 package com.example.plusgo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.plusgo.UPM.AddPreferenceActivity;
+import com.example.plusgo.UPM.NewUserActivity;
 
 public class Login extends AppCompatActivity {
 
+    private Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        login = (Button) findViewById(R.id.btnLog);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, NewUserActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- Plusgo/app/src/main/java/com/example/plusgo/UPM/NewUserActivity.java
+++ Plusgo/app/src/main/java/com/example/plusgo/UPM/NewUserActivity.java
@@ -8,16 +8,40 @@
 
 package com.example.plusgo.UPM;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.Spinner;
 
 import com.example.plusgo.R;
 
 public class NewUserActivity extends AppCompatActivity {
-
+    private Spinner profession;
+    private ImageButton goToPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_user);
+
+        profession = findViewById(R.id.profession);
+        String[] prof = new String[]{"Driver", "Body Guard", "Security Officer","Clerical Staff", "Clerk", "Intern", "Administrative Assistant","Associate Engineer","Bank Assistant","IT Support",
+                "Cashier","Network Engineer","Software Engineer","Database Administrator", "Project Manager","HR","Nurse","Lecturer","Teacher",
+                "Tech Lead","Doctor","Lawyer","Professor","Senior Lecturer","Senior Lead","Senior Accountant","CEO","CIO","IT Director","Manager"};
+        ArrayAdapter<String> adapterProf = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, prof);
+        profession.setAdapter(adapterProf);
+
+        goToPreferences = (ImageButton)findViewById(R.id.btnPreference);
+        goToPreferences.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent preferences = new Intent(NewUserActivity.this, AddPreferenceActivity.class);
+                startActivity(preferences);
+            }
+        });
+
+
     }
 }
