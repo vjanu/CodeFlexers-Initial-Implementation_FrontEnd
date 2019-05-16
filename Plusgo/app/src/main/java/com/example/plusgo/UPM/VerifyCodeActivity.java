64e99a6e9eefaeef9094b19fcf4a79e1a8979bff
vjanu <vjanuradhawick@gmail.com>
2019-05-16 21:58:29 +0530

    Gradle version change

--- Plusgo/app/src/main/java/com/example/plusgo/UPM/VerifyCodeActivity.java
+++ Plusgo/app/src/main/java/com/example/plusgo/UPM/VerifyCodeActivity.java
@@ -2,20 +2,20 @@
  * *
  *  * Created by Viraj Wickramasinghe
  *  * Copyright (c) 2019 . All rights reserved.
- *  * Last modified 4/17/19 2:26 PM
+ *  * Last modified 5/15/19 6:22 PM
  *
  */
 
 package com.example.plusgo.UPM;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.plusgo.R;
-import com.example.plusgo.Signup;
+import com.example.plusgo.SignUp;
 
 public class VerifyCodeActivity extends AppCompatActivity {
 
@@ -29,7 +29,7 @@ public class VerifyCodeActivity extends AppCompatActivity {
         btnVerify.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent verify = new Intent(VerifyCodeActivity.this, Signup.class);
+                Intent verify = new Intent(VerifyCodeActivity.this, SignUp.class);
                 startActivity(verify);
 
             }
