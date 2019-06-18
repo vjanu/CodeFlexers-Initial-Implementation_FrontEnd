ff7ee9dcf10772e10dae7e1a312321a80abcca6c
athnath <32517652+athnath@users.noreply.github.com>
2019-06-18 19:54:11 +0530

    add new traffic jam file

---
+++ Plusgo/app/src/main/java/com/example/plusgo/OPR/TrafficJam.java
@@ -0,0 +1,376 @@
+/*
+ * *
+ *  * Created by Athrie
+ *  * Copyright (c) 2019 . All rights reserved.
+ *  * Last modified 5/9/19 7:12 PM
+ *
+ */
+
+package com.example.plusgo.OPR;
+
+import android.Manifest;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.plusgo.R;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+public class TrafficJam extends AppCompatActivity {
+/*
+    ImageView xImageview;
+    Button addimage;
+
+
+    private static final int IMAGE_PICK_CODE =1000;
+    private static final int PERMISSION_CODE =1001;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_traffic_jam);
+        xImageview=findViewById(R.id.imageViewaddimg);
+        addimage=findViewById(R.id.add_img_button);
+
+        addimage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M)
+                {
+
+                    if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)== PackageManager.PERMISSION_DENIED){
+                        String [] permissions={Manifest.permission.READ_EXTERNAL_STORAGE};
+                        requestPermissions(permissions,PERMISSION_CODE);
+                    }
+
+                    else{
+                        pickImageFromGallery();
+                    }
+                }
+                else{
+                    pickImageFromGallery();
+                }
+            }
+        });
+
+    }
+
+
+    private void pickImageFromGallery(){
+
+        Intent intent =new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent,IMAGE_PICK_CODE);
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        switch (requestCode){
+            case PERMISSION_CODE:{
+                if(grantResults.length>0 && grantResults[0]== PackageManager.PERMISSION_GRANTED){
+                    pickImageFromGallery();
+                }
+                else{
+                    Toast.makeText(this,"Permission denied !",Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(resultCode==RESULT_OK && requestCode ==IMAGE_PICK_CODE){
+            //set image to image view
+            xImageview.setImageURI(data.getData());
+        }
+    }
+
+
+*/
+private final int PICK_IMAGE_CAMERA = 1, PICK_IMAGE_GALLERY = 2;
+    ImageView image_view;
+    Button btnlater;
+    Button btnreport;
+    Button btn_upload_traffic;
+    TextView input_location;
+    RadioButton radio_standstill;
+    RadioButton radio_moderate;
+    RadioButton radio_heavy;
+    //String URL = "http://192.168.8.100:8085/roadcause/";
+    String URL="http://192.168.43.88:8083/roadcause/";
+    RequestQueue rQueue;
+    private String STATUS_ACTIVE = "ACTIVE", STATUS_INACTIVE = "INACTIVE", TYPE = "TRAFIC", SITUATION; // 1-> standstill, 2-> moderate, 3-> heavy
+    private Bitmap bitmap;
+    private File destination = null;
+    private InputStream inputStreamImg;
+    private String imgPath = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_traffic_jam);
+
+        image_view = findViewById(R.id.image_view_traffic);
+        btnlater = findViewById(R.id.btnlater);
+        btnreport = findViewById(R.id.btnreport);
+        input_location = findViewById(R.id.input_location);
+        radio_standstill = findViewById(R.id.radio_standstill);
+        radio_moderate = findViewById(R.id.radio_moderate);
+        radio_heavy = findViewById(R.id.radio_heavy);
+
+        btn_upload_traffic = findViewById(R.id.btn_upload_traffic);
+        btn_upload_traffic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImage();
+            }
+        });
+
+        btnreport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                insertRoadCause(STATUS_ACTIVE);
+            }
+        });
+
+        btnlater.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                insertRoadCause(STATUS_INACTIVE);
+            }
+        });
+
+        final boolean flag_standstill = false, flag_moderate = false, flag_heavy = false;
+
+        radio_standstill.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (radio_standstill.isChecked()) {
+                    SITUATION = "1";
+                }
+            }
+        });
+
+        radio_moderate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (radio_moderate.isChecked()) {
+                    SITUATION = "2";
+                }
+            }
+        });
+
+        radio_heavy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (radio_heavy.isChecked()) {
+                    SITUATION = "3";
+                }
+            }
+        });
+
+    }
+
+
+    // Select image from camera and gallery
+    private void selectImage() {
+        try {
+            PackageManager pm = getPackageManager();
+            int hasPerm = pm.checkPermission(Manifest.permission.CAMERA, getPackageName());
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                    == PackageManager.PERMISSION_DENIED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 0);
+            } else {
+                if (hasPerm == PackageManager.PERMISSION_GRANTED) {
+                    final CharSequence[] options = {"Take Photo", "Choose From Gallery", "Cancel"};
+                    android.support.v7.app.AlertDialog.Builder builder = new android.support.v7.app.AlertDialog.Builder(this);
+                    builder.setTitle("Select Option");
+                    builder.setItems(options, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int item) {
+                            if (options[item].equals("Take Photo")) {
+                                dialog.dismiss();
+                                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                                startActivityForResult(intent, PICK_IMAGE_CAMERA);
+                            } else if (options[item].equals("Choose From Gallery")) {
+                                dialog.dismiss();
+                                Intent pickPhoto = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                                startActivityForResult(pickPhoto, PICK_IMAGE_GALLERY);
+                            } else if (options[item].equals("Cancel")) {
+                                dialog.dismiss();
+                            }
+                        }
+                    });
+                    builder.show();
+                } else
+                    Toast.makeText(this, "Camera Permission error", Toast.LENGTH_SHORT).show();
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, "Camera Permission error", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        inputStreamImg = null;
+        if (requestCode == PICK_IMAGE_CAMERA) {
+            try {
+                Uri selectedImage = data.getData();
+                bitmap = (Bitmap) data.getExtras().get("data");
+                ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 50, bytes);
+
+                Log.e("Activity", "Pick from Camera::>>> ");
+
+                String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+                destination = new File(Environment.getExternalStorageDirectory() + "/" +
+                        getString(R.string.app_name), "IMG_" + timeStamp + ".jpg");
+                FileOutputStream fo;
+                try {
+                    destination.createNewFile();
+                    fo = new FileOutputStream(destination);
+                    fo.write(bytes.toByteArray());
+                    fo.close();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                imgPath = destination.getAbsolutePath();
+                image_view.setImageBitmap(bitmap);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else if (requestCode == PICK_IMAGE_GALLERY) {
+            Uri selectedImage = data.getData();
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+                ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 50, bytes);
+                Log.e("Activity", "Pick from Gallery::>>> ");
+
+                imgPath = getRealPathFromURI(selectedImage);
+                destination = new File(imgPath.toString());
+                image_view.setImageBitmap(bitmap);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public String getRealPathFromURI(Uri contentUri) {
+        String[] proj = {MediaStore.Audio.Media.DATA};
+        Cursor cursor = managedQuery(contentUri, proj, null, null, null);
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA);
+        cursor.moveToFirst();
+        return cursor.getString(column_index);
+    }
+
+    private void insertRoadCause(final String status) {
+
+        final Dialog dialog = new Dialog(TrafficJam.this);
+        dialog.setContentView(R.layout.spinner);
+        dialog.setTitle("Uploading ...");
+        dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+        dialog.setCancelable(false);
+
+        // set the custom dialog components - text, image and button
+
+        ProgressBar progressBar = (ProgressBar) dialog.findViewById(R.id.Circle);
+        //Sprite animation = new FoldingCube();
+        //rogressBar.setIndeterminateDrawable(animation);
+
+        dialog.show();
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+        final String encoded_image = Base64.encodeToString(byteArrayOutputStream.toByteArray(), Base64.DEFAULT);
+
+        StringRequest sr = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                rQueue.getCache().clear();
+                dialog.cancel();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                dialog.cancel();
+                rQueue.getCache().clear();
+                Log.e("error", error.toString());
+                Toast.makeText(getApplicationContext(), "Upload Failed", Toast.LENGTH_LONG).show();
+
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("type", TYPE);
+                params.put("username", "aaarrrrrrraa");
+                params.put("destination", input_location.getText().toString());
+                params.put("situation", SITUATION);
+                params.put("latitude", "34343");
+                params.put("longitude", "43434");
+                params.put("image", encoded_image);
+                params.put("status", status);
+                return params;
+            }
+        };
+        sr.setRetryPolicy(new DefaultRetryPolicy(
+                0,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+
+        rQueue = Volley.newRequestQueue(TrafficJam.this);
+        rQueue.add(sr);
+
+    }
+
+
+}
