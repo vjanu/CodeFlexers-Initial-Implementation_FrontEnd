947e6b95accc0e6b61d9c2be854b84e7ff3282bf
athnath <32517652+athnath@users.noreply.github.com>
2019-06-20 14:16:43 +0530

    add map feature

---
+++ Plusgo/app/src/main/java/com/example/plusgo/OPR/MainActivity.java
@@ -0,0 +1,289 @@
+package com.example.plusgo.OPR;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.example.plusgo.BuildConfig;
+import com.example.plusgo.R;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationServices;
+
+import org.osmdroid.bonuspack.routing.OSRMRoadManager;
+import org.osmdroid.bonuspack.routing.Road;
+import org.osmdroid.bonuspack.routing.RoadManager;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.events.DelayedMapListener;
+import org.osmdroid.events.MapListener;
+import org.osmdroid.events.ScrollEvent;
+import org.osmdroid.events.ZoomEvent;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.views.overlay.Polyline;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+
+    private static final int MULTIPLE_PERMISSION_REQUEST_CODE = 4;
+    private MapView mapView;
+    private Location mLastLocation;
+    private GoogleApiClient mGoogleApiClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_map);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+
+        checkPermissionsState();
+    }
+
+    private void checkPermissionsState() {
+        int internetPermissionCheck = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.INTERNET);
+
+        int networkStatePermissionCheck = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_NETWORK_STATE);
+
+        int writeExternalStoragePermissionCheck = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        int coarseLocationPermissionCheck = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION);
+
+        int fineLocationPermissionCheck = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+
+        int wifiStatePermissionCheck = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_WIFI_STATE);
+
+        if (internetPermissionCheck == PackageManager.PERMISSION_GRANTED &&
+                networkStatePermissionCheck == PackageManager.PERMISSION_GRANTED &&
+                writeExternalStoragePermissionCheck == PackageManager.PERMISSION_GRANTED &&
+                coarseLocationPermissionCheck == PackageManager.PERMISSION_GRANTED &&
+                fineLocationPermissionCheck == PackageManager.PERMISSION_GRANTED &&
+                wifiStatePermissionCheck == PackageManager.PERMISSION_GRANTED) {
+
+            setupMap();
+
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{
+                            Manifest.permission.INTERNET,
+                            Manifest.permission.ACCESS_NETWORK_STATE,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                            Manifest.permission.ACCESS_COARSE_LOCATION,
+                            Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_WIFI_STATE},
+                    MULTIPLE_PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MULTIPLE_PERMISSION_REQUEST_CODE: {
+                if (grantResults.length > 0) {
+                    boolean somePermissionWasDenied = false;
+                    for (int result : grantResults) {
+                        if (result == PackageManager.PERMISSION_DENIED) {
+                            somePermissionWasDenied = true;
+                        }
+                    }
+                    if (somePermissionWasDenied) {
+                        Toast.makeText(this, "Cant load maps without all the permissions granted", Toast.LENGTH_SHORT).show();
+                    } else {
+                        setupMap();
+                    }
+                } else {
+                    Toast.makeText(this, "Cant load maps without all the permissions granted", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+
+        }
+    }
+
+    private void setupMap() {
+
+        final Context ctx = getApplicationContext();
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+        Configuration.getInstance().setUserAgentValue(BuildConfig.APPLICATION_ID);
+        mapView = (MapView) findViewById(R.id.mapview);
+        //setContentView(mapView); //displaying the MapView
+
+        mapView.getController().setZoom(20); //set initial zoom-level, depends on your need
+        //mapView.getController().setCenter(ONCATIVO);
+        //mapView.setUseDataConnection(false); //keeps the mapView from loading online tiles using network connection.
+        mapView.setTileSource(TileSourceFactory.MAPNIK);
+        mapView.setClickable(true);
+        mapView.setBuiltInZoomControls(false);
+        mapView.setMultiTouchControls(true);
+
+        MyLocationNewOverlay oMapLocationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(ctx), mapView);
+        mapView.getOverlays().add(oMapLocationOverlay);
+        oMapLocationOverlay.enableFollowLocation();
+        oMapLocationOverlay.enableMyLocation();
+        oMapLocationOverlay.enableFollowLocation();
+
+//        CompassOverlay compassOverlay = new CompassOverlay(this, mapView);
+//        compassOverlay.enableCompass();
+//        mapView.getOverlays().add(compassOverlay);
+
+        mapView.setMapListener(new DelayedMapListener(new MapListener() {
+            public boolean onZoom(final ZoomEvent e) {
+                MapView mapView = (MapView) findViewById(R.id.mapview);
+
+                String latitudeStr = "" + mapView.getMapCenter().getLatitude();
+                String longitudeStr = "" + mapView.getMapCenter().getLongitude();
+
+                String latitudeFormattedStr = latitudeStr.substring(0, Math.min(latitudeStr.length(), 7));
+                String longitudeFormattedStr = longitudeStr.substring(0, Math.min(longitudeStr.length(), 7));
+
+                Log.i("zoom", "" + mapView.getMapCenter().getLatitude() + ", " + mapView.getMapCenter().getLongitude());
+//                TextView latLongTv = (TextView) findViewById(R.id.textView);
+//                latLongTv.setText("" + latitudeFormattedStr + ", " + longitudeFormattedStr);
+                return true;
+            }
+
+            public boolean onScroll(final ScrollEvent e) {
+                MapView mapView = (MapView) findViewById(R.id.mapview);
+
+                String latitudeStr = "" + mapView.getMapCenter().getLatitude();
+                String longitudeStr = "" + mapView.getMapCenter().getLongitude();
+
+                String latitudeFormattedStr = latitudeStr.substring(0, Math.min(latitudeStr.length(), 7));
+                String longitudeFormattedStr = longitudeStr.substring(0, Math.min(longitudeStr.length(), 7));
+
+                Log.i("scroll", "" + mapView.getMapCenter().getLatitude() + ", " + mapView.getMapCenter().getLongitude());
+//                TextView latLongTv = (TextView) findViewById(R.id.textView);
+//                latLongTv.setText("" + latitudeFormattedStr + ", " + longitudeFormattedStr);
+                return true;
+            }
+        }, 1000));
+
+        RoadManager roadManager = new OSRMRoadManager(this);
+
+        ArrayList<GeoPoint> waypoints = new ArrayList<>();
+        GeoPoint startPoint = new GeoPoint(7.009618, 79.965317);
+        waypoints.add(startPoint);
+
+        Marker startMarker = new Marker(mapView);
+        startMarker.setPosition(startPoint);
+        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        startMarker.setIcon(getResources().getDrawable(R.drawable.ic_pin));
+
+        GeoPoint endPoint = new GeoPoint(6.927079, 79.861244);
+        waypoints.add(endPoint);
+
+        Marker endMarker = new Marker(mapView);
+        endMarker.setPosition(endPoint);
+        endMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        endMarker.setIcon(getResources().getDrawable(R.drawable.ic_pin));
+
+        Road road = roadManager.getRoad(waypoints);
+        Polyline roadOverlay = RoadManager.buildRoadOverlay(road);
+        mapView.getOverlays().add(startMarker);
+        mapView.getOverlays().add(endMarker);
+        mapView.getOverlays().add(roadOverlay);
+
+//        BoundingBox boundingBox = new BoundingBox();
+//        boundingBox.set(startPoint.getLatitude(), startPoint.getLatitude(), endPoint.getLatitude(), endPoint.getLongitude());
+//        mapView.zoomToBoundingBox(boundingBox, true);
+
+        mapView.invalidate();
+    }
+
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    private void setCenterInMyCurrentLocation() {
+        if (mLastLocation != null) {
+            mapView.getController().setCenter(new GeoPoint(mLastLocation.getLatitude(), mLastLocation.getLongitude()));
+        } else {
+            Toast.makeText(this, "Getting current location", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        } else if (id == R.id.action_locate) {
+            setCenterInMyCurrentLocation();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mLastLocation = location;
+    }
+}
