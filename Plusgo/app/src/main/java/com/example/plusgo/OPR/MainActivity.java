58071ac26cd3e36bcbf032f680edf44ae807ecfe
athnath <32517652+athnath@users.noreply.github.com>
2019-06-22 00:00:29 +0530

    add long,lati

--- Plusgo/app/src/main/java/com/example/plusgo/OPR/MainActivity.java
+++ Plusgo/app/src/main/java/com/example/plusgo/OPR/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.plusgo.OPR;
 
 import android.Manifest;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
@@ -14,14 +16,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.plusgo.BuildConfig;
 import com.example.plusgo.R;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.widget.Autocomplete;
+import com.google.android.libraries.places.widget.AutocompleteActivity;
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
 
 import org.osmdroid.bonuspack.routing.OSRMRoadManager;
 import org.osmdroid.bonuspack.routing.Road;
@@ -40,13 +60,28 @@ import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+//import com.google.android.gms.location.places.Place;
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
     private static final int MULTIPLE_PERMISSION_REQUEST_CODE = 4;
-    private MapView mapView;
+    int AUTOCOMPLETE_REQUEST_CODE = 1;
+    RequestQueue rQueue;
+  //  String URL = "http://192.168.8.102:8083/map";
+    String URL = "http://192.168.1.4:8083/map";
     private Location mLastLocation;
     private GoogleApiClient mGoogleApiClient;
+    private MapView mapView;
+    private EditText source_location, source_destination;
+    private Button find_my_driver, accept_route;
+    private String TAG = "RequestRide";
+    private double source_lat, source_long, destination_lat, destination_long;
+    private boolean source, destination;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +89,8 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         setContentView(R.layout.activity_main_map);
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
+
+        Places.initialize(getApplicationContext(), "AIzaSyD_gxGkVFP_a2v0_VopWLlvFZe-u46TV7M"); //Please put application code in a secure place
 
         if (android.os.Build.VERSION.SDK_INT > 9) {
             StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
@@ -68,7 +105,162 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
                     .build();
         }
 
+        find_my_driver = findViewById(R.id.btnfinddriver);
+        accept_route = findViewById(R.id.btnacceptroute);
+
+        source_location = findViewById(R.id.source_location);
+        source_destination = findViewById(R.id.source_destination);
+
+        source_location.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                source_location.setPressed(true);
+                source_location.setSelection(source_location.getText().length());
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    source = true;
+                    destination = false;
+                    drawAutocompleteIntent();
+                }
+                source_destination.getText().clear();
+//                mapView.getOverlays().clear();
+                return false;
+            }
+        });
+
+
+        source_destination.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                source_destination.setPressed(true);
+                source_destination.setSelection(source_destination.getText().length());
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    source = false;
+                    destination = true;
+                    drawAutocompleteIntent();
+                }
+                return false;
+            }
+        });
+
+        accept_route.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                insertMapData();
+            }
+        });
+
         checkPermissionsState();
+    }
+
+    private void drawAutocompleteIntent() {
+        // Set the fields to specify which types of place data to
+        List<Place.Field> fields = Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.LAT_LNG);
+
+        // Start the autocomplete intent.
+        Intent intent = new Autocomplete.IntentBuilder(
+                AutocompleteActivityMode.FULLSCREEN, fields)
+                .build(MainActivity.this);
+        startActivityForResult(intent, AUTOCOMPLETE_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == AUTOCOMPLETE_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Place place = Autocomplete.getPlaceFromIntent(data);
+                Log.i(TAG, "Place: " + place.getName() + ", " + place.getId());
+
+                if (place != null && source) {
+                    LatLng latLng = place.getLatLng();
+                    source_lat = latLng.latitude;
+                    source_long = latLng.longitude;
+                    source_location.setText(place.getName());
+                }
+
+                if (place != null && destination) {
+                    LatLng latLng = place.getLatLng();
+                    destination_lat = latLng.latitude;
+                    destination_long = latLng.longitude;
+                    source_destination.setText(place.getName());
+                    drawMap();
+                }
+
+            } else if (resultCode == AutocompleteActivity.RESULT_ERROR) {
+                // TODO: Handle the error.
+                Status status = Autocomplete.getStatusFromIntent(data);
+                Log.i(TAG, status.getStatusMessage());
+            } else if (resultCode == RESULT_CANCELED) {
+                // The user canceled the operation.
+            }
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MULTIPLE_PERMISSION_REQUEST_CODE: {
+                if (grantResults.length > 0) {
+                    boolean somePermissionWasDenied = false;
+                    for (int result : grantResults) {
+                        if (result == PackageManager.PERMISSION_DENIED) {
+                            somePermissionWasDenied = true;
+                        }
+                    }
+                    if (somePermissionWasDenied) {
+                        Toast.makeText(this, "Cant load maps without all the permissions granted", Toast.LENGTH_SHORT).show();
+                    } else {
+                        setupMap();
+                    }
+                } else {
+                    Toast.makeText(this, "Cant load maps without all the permissions granted", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        } else if (id == R.id.action_locate) {
+            setCenterInMyCurrentLocation();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mLastLocation = location;
     }
 
     private void checkPermissionsState() {
@@ -109,31 +301,6 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
                             Manifest.permission.ACCESS_FINE_LOCATION,
                             Manifest.permission.ACCESS_WIFI_STATE},
                     MULTIPLE_PERMISSION_REQUEST_CODE);
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case MULTIPLE_PERMISSION_REQUEST_CODE: {
-                if (grantResults.length > 0) {
-                    boolean somePermissionWasDenied = false;
-                    for (int result : grantResults) {
-                        if (result == PackageManager.PERMISSION_DENIED) {
-                            somePermissionWasDenied = true;
-                        }
-                    }
-                    if (somePermissionWasDenied) {
-                        Toast.makeText(this, "Cant load maps without all the permissions granted", Toast.LENGTH_SHORT).show();
-                    } else {
-                        setupMap();
-                    }
-                } else {
-                    Toast.makeText(this, "Cant load maps without all the permissions granted", Toast.LENGTH_SHORT).show();
-                }
-                return;
-            }
-
         }
     }
 
@@ -195,10 +362,14 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
             }
         }, 1000));
 
+
+    }
+
+    private void drawMap() {
         RoadManager roadManager = new OSRMRoadManager(this);
 
         ArrayList<GeoPoint> waypoints = new ArrayList<>();
-        GeoPoint startPoint = new GeoPoint(7.009618, 79.965317);
+        GeoPoint startPoint = new GeoPoint(source_lat, source_long);
         waypoints.add(startPoint);
 
         Marker startMarker = new Marker(mapView);
@@ -206,7 +377,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
         startMarker.setIcon(getResources().getDrawable(R.drawable.ic_pin));
 
-        GeoPoint endPoint = new GeoPoint(6.927079, 79.861244);
+        GeoPoint endPoint = new GeoPoint(destination_lat, destination_long);
         waypoints.add(endPoint);
 
         Marker endMarker = new Marker(mapView);
@@ -245,45 +416,54 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         }
     }
 
-    @Override
-    public void onConnected(Bundle connectionHint) {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return;
-        }
-        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-    }
+    private void insertMapData() {
 
-    @Override
-    public void onConnectionSuspended(int i) {
+        final Dialog dialog = new Dialog(MainActivity.this);
+        dialog.setContentView(R.layout.spinner);
+        dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+        dialog.setCancelable(false);
 
-    }
+        // set the custom dialog components - text, image and button
 
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+      //  ProgressBar progressBar = dialog.findViewById(R.id.Circle);
+        //Sprite animation = new Circle();
+        //progressBar.setIndeterminateDrawable(animation);
 
-    }
+        dialog.show();
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        StringRequest sr = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                rQueue.getCache().clear();
+                dialog.cancel();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                dialog.cancel();
+                rQueue.getCache().clear();
+                Log.e("error", error.toString());
+                Toast.makeText(getApplicationContext(), "Task Failed", Toast.LENGTH_LONG).show();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("userid", "nathasha");
+                params.put("source_long", String.valueOf(source_long));
+                params.put("source_lat", String.valueOf(source_lat));
+                params.put("destination_long", String.valueOf(destination_long));
+                params.put("destination_lat", String.valueOf(destination_lat));
+                return params;
+            }
+        };
+        sr.setRetryPolicy(new DefaultRetryPolicy(
+                0,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
+        rQueue = Volley.newRequestQueue(MainActivity.this);
+        rQueue.add(sr);
 
-        if (id == R.id.action_settings) {
-            return true;
-        } else if (id == R.id.action_locate) {
-            setCenterInMyCurrentLocation();
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-        mLastLocation = location;
     }
 }
