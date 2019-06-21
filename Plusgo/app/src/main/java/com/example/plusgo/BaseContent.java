eb3389105638995920f8ad2bf79d2c23b2755b10
vediras <ashene.edirisinghe@pearson.com>
2019-06-22 01:46:38 +0530

    tested with ubuntu machine for all functions

--- Plusgo/app/src/main/java/com/example/plusgo/BaseContent.java
+++ Plusgo/app/src/main/java/com/example/plusgo/BaseContent.java
@@ -4,14 +4,16 @@ package com.example.plusgo;
 public class BaseContent {
 
     //TODO : ADD THE IP OF SERVER HERE
-    final public String BASEIPROUTE = "http://10.98.200.109";
+    final public String BASEIPROUTE = "http://192.168.43.189";
 
     //IP ADDRESS OF THE NODEJS BACKEND
     public String IpAddress = BASEIPROUTE+":8083";
 
+    //TODO : PORTS 8088,8089,8090 - USED IN DVPRM
     //for image upload service written in php
-//    public String phpIP = "http://192.168.1.4:8080";
-    public String phpIP = BASEIPROUTE+":8080";
+    //public String phpIP = "http://192.168.1.4:80";
+    public String phpIP = BASEIPROUTE+":80";
+
 
 //    public String pythonIpAddress = "http://192.168.1.4:99";
     public String pythonIpAddress = BASEIPROUTE+":99";//TODO : INSERT WHOS COMPONENT IS RELATED TO THIS PORT
