953eea04f21168b3965830367e6a52b2427294a9
vediras <ashene.edirisinghe@pearson.com>
2019-06-23 19:57:30 +0530

    button model changed

--- Plusgo/app/src/main/java/com/example/plusgo/DVPRM/RatingDriverActivity.java
+++ Plusgo/app/src/main/java/com/example/plusgo/DVPRM/RatingDriverActivity.java
@@ -278,7 +278,7 @@ public class RatingDriverActivity extends AppCompatActivity {
     //Retrieve sentiment rating
     private void jsonrequestforsentimentcheck(String sentiment) {
 
-        String JSON_URL = BASECONTENT.IpAddress +":8090/sentiment/"+sentiment;
+        String JSON_URL = BASECONTENT.DVPRMBASEIPROUTE +":8090/sentiment/"+sentiment;
 
         final String senti = sentiment;
         final String finalJSON_URL = JSON_URL;
