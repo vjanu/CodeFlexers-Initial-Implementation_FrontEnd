1cef651b788c195ea6f131cf7526dac6e5b7512b
athnath <32517652+athnath@users.noreply.github.com>
2019-07-01 08:54:33 +0530

    add driver map

--- Plusgo/app/src/main/java/com/example/plusgo/FC/FragmentMap.java
+++ Plusgo/app/src/main/java/com/example/plusgo/FC/FragmentMap.java
@@ -1,28 +1,293 @@
 package com.example.plusgo.FC;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.plusgo.BuildConfig;
 import com.example.plusgo.R;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationServices;
 
-public class FragmentMap extends Fragment {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.events.DelayedMapListener;
+import org.osmdroid.events.MapListener;
+import org.osmdroid.events.ScrollEvent;
+import org.osmdroid.events.ZoomEvent;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
-    View view;
+import java.util.ArrayList;
+
+public class FragmentMap extends Fragment implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+
+
+//    View view;
+//
+//    @Override
+//    public void onCreate(@Nullable Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//    }
+//
+//    @Nullable
+//    @Override
+//    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+//      //  view = inflater.inflate(R.layout.activity_fragment_map,container,false);
+//        return view;
+//    }
+
+
+    private static final int MULTIPLE_PERMISSION_REQUEST_CODE = 4;
+    View rootView;
+    private Location mLastLocation;
+    private GoogleApiClient mGoogleApiClient;
+    private MapView mapView;
+    private String TAG = "DriverFragment";
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
-    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.activity_fragment_map,container,false);
-        return view;
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.activity_fragment_map, container, false);
+
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(getActivity().getApplicationContext())
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+
+        checkPermissionsState();
+        return rootView;
     }
+
+    @Override
+    public void onActivityCreated (Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        checkPermissionsState();
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        if (ActivityCompat.checkSelfPermission(getActivity().getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity().getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        } else if (id == R.id.action_locate) {
+            setCenterInMyCurrentLocation();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mLastLocation = location;
+    }
+
+    private void checkPermissionsState() {
+        int internetPermissionCheck = ContextCompat.checkSelfPermission(getActivity().getApplicationContext(),
+                Manifest.permission.INTERNET);
+
+        int networkStatePermissionCheck = ContextCompat.checkSelfPermission(getActivity().getApplicationContext(),
+                Manifest.permission.ACCESS_NETWORK_STATE);
+
+        int writeExternalStoragePermissionCheck = ContextCompat.checkSelfPermission(getActivity().getApplicationContext(),
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        int coarseLocationPermissionCheck = ContextCompat.checkSelfPermission(getActivity().getApplicationContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION);
+
+        int fineLocationPermissionCheck = ContextCompat.checkSelfPermission(getActivity().getApplicationContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION);
+
+        int wifiStatePermissionCheck = ContextCompat.checkSelfPermission(getActivity().getApplicationContext(),
+                Manifest.permission.ACCESS_WIFI_STATE);
+
+        if (internetPermissionCheck == PackageManager.PERMISSION_GRANTED &&
+                networkStatePermissionCheck == PackageManager.PERMISSION_GRANTED &&
+                writeExternalStoragePermissionCheck == PackageManager.PERMISSION_GRANTED &&
+                coarseLocationPermissionCheck == PackageManager.PERMISSION_GRANTED &&
+                fineLocationPermissionCheck == PackageManager.PERMISSION_GRANTED &&
+                wifiStatePermissionCheck == PackageManager.PERMISSION_GRANTED) {
+
+            setupMap();
+
+        } else {
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{
+                            Manifest.permission.INTERNET,
+                            Manifest.permission.ACCESS_NETWORK_STATE,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                            Manifest.permission.ACCESS_COARSE_LOCATION,
+                            Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_WIFI_STATE},
+                    MULTIPLE_PERMISSION_REQUEST_CODE);
+            checkPermissionsState();
+        }
+    }
+
+    private void setupMap() {
+
+        final Context ctx = getActivity().getApplicationContext();
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+        Configuration.getInstance().setUserAgentValue(BuildConfig.APPLICATION_ID);
+        mapView = (MapView) rootView.findViewById(R.id.mapviewdriver);
+
+        mapView.getController().setZoom(20); //set initial zoom-level, depends on your need
+        //mapView.getController().setCenter(ONCATIVO);
+        //mapView.setUseDataConnection(false); //keeps the mapView from loading online tiles using network connection.
+        mapView.setTileSource(TileSourceFactory.MAPNIK);
+        mapView.setClickable(true);
+        mapView.setBuiltInZoomControls(false);
+        mapView.setMultiTouchControls(true);
+
+        MyLocationNewOverlay oMapLocationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(ctx), mapView);
+        mapView.getOverlays().add(oMapLocationOverlay);
+        oMapLocationOverlay.enableFollowLocation();
+        oMapLocationOverlay.enableMyLocation();
+        oMapLocationOverlay.enableFollowLocation();
+
+
+        mapView.setMapListener(new DelayedMapListener(new MapListener() {
+            public boolean onZoom(final ZoomEvent e) {
+                MapView mapView = (MapView) getActivity().findViewById(R.id.mapviewdriver);
+
+                String latitudeStr = "" + mapView.getMapCenter().getLatitude();
+                String longitudeStr = "" + mapView.getMapCenter().getLongitude();
+
+                String latitudeFormattedStr = latitudeStr.substring(0, Math.min(latitudeStr.length(), 7));
+                String longitudeFormattedStr = longitudeStr.substring(0, Math.min(longitudeStr.length(), 7));
+
+                Log.i("zoom", "" + mapView.getMapCenter().getLatitude() + ", " + mapView.getMapCenter().getLongitude());
+//                TextView latLongTv = (TextView) findViewById(R.id.textView);
+//                latLongTv.setText("" + latitudeFormattedStr + ", " + longitudeFormattedStr);
+                return true;
+            }
+
+            public boolean onScroll(final ScrollEvent e) {
+                MapView mapView = (MapView) getActivity().findViewById(R.id.mapviewdriver);
+
+                String latitudeStr = "" + mapView.getMapCenter().getLatitude();
+                String longitudeStr = "" + mapView.getMapCenter().getLongitude();
+
+                String latitudeFormattedStr = latitudeStr.substring(0, Math.min(latitudeStr.length(), 7));
+                String longitudeFormattedStr = longitudeStr.substring(0, Math.min(longitudeStr.length(), 7));
+
+                Log.i("scroll", "" + mapView.getMapCenter().getLatitude() + ", " + mapView.getMapCenter().getLongitude());
+//                TextView latLongTv = (TextView) findViewById(R.id.textView);
+//                latLongTv.setText("" + latitudeFormattedStr + ", " + longitudeFormattedStr);
+                return true;
+            }
+        }, 1000));
+        getPassengerLocationData();
+    }
+
+    public void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    public void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    private void setCenterInMyCurrentLocation() {
+        if (mLastLocation != null) {
+            mapView.getController().setCenter(new GeoPoint(mLastLocation.getLatitude(), mLastLocation.getLongitude()));
+        } else {
+            Toast.makeText(getActivity().getApplicationContext(), "Getting current location", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void getPassengerLocationData() {
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest("http://192.168.1.4:8083/map/passenger/U1558711443502", new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+
+                ArrayList<GeoPoint> waypoints = new ArrayList<>();
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        JSONObject jsonObject = response.getJSONObject(i);
+                        double starting_lat = jsonObject.getDouble("StartingLat");
+                        double starting_long = jsonObject.getDouble("StartingLong");
+                        GeoPoint geoPoint = new GeoPoint(starting_lat, starting_long);
+                        Marker marker = new Marker(mapView);
+                        marker.setPosition(geoPoint);
+                        marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+                        marker.setIcon(getResources().getDrawable(R.drawable.passenger));
+                        mapView.getOverlays().add(marker);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+                mapView.invalidate();
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, error.toString());
+            }
+        });
+        RequestQueue requestQueue = Volley.newRequestQueue(getActivity().getApplicationContext());
+        requestQueue.add(jsonArrayRequest);
+
+    }
+
 }
